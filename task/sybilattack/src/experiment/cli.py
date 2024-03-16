f9a9261195782ec724b7a668f36897f14dd57aa7
Adam Krawczyk <adamkrawczyk@MacBook-Pro-Adam.local>
2024-03-16 20:01:35 +0100

    Affine train

---
+++ task/sybilattack/src/experiment/cli.py
@@ -0,0 +1,11 @@
+from lightning.pytorch.cli import LightningCLI
+
+
+class SybilCLI(LightningCLI):
+    def add_arguments_to_parser(self, parser):
+        parser.link_arguments(("data.source_transform", "data.transform_type"),
+                              "model.predict_prefix",
+                              compute_fn=lambda source_transform, transform_type: f"{source_transform}_{transform_type}",)
+        parser.link_arguments(("data.source_transform", "data.target_transform"),
+                              "model.same",
+                              compute_fn=lambda source_transform, target_transform: source_transform == target_transform,)
